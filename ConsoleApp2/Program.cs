926be1e1cf3a72565a4e74d0525654d96ecdd3df
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-30 19:14:38 +0000

    Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine($"Git Repository: {Sourcy.Git.RootDirectory}");
+Console.WriteLine($"Solution: {Sourcy.DotNet.Solutions.ConsoleApp2}");
+Console.WriteLine($"Project: {Sourcy.DotNet.Projects.ConsoleApp2}");
